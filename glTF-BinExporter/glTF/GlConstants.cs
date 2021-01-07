744a7acbe15f773ab62a36812b5de826206d3719
Joshua Kennedy <joshuakennedy102@gmail.com>
2021-01-07 13:30:34 +0200

    Resolves incorrect constants for sampler and const corrects the GlConstants class

--- glTF-BinExporter/glTF/GlConstants.cs
+++ glTF-BinExporter/glTF/GlConstants.cs
@@ -7,47 +7,50 @@ namespace glTF_BinExporter.glTF
     {
         // componentType
         // Byte size = 1
-        public static int BYTE = 5120;
-        public static int UNSIGNED_BYTE = 5121;
+        public const int BYTE = 5120;
+        public const int UNSIGNED_BYTE = 5121;
         // Byte size = 2
-        public static int SHORT = 5122;
-        public static int UNSIGNED_SHORT = 5123;
+        public const int SHORT = 5122;
+        public const int UNSIGNED_SHORT = 5123;
         // Byte size = 4
-        public static int UNSIGNED_INT = 5125;
-        public static int FLOAT = 5126;
+        public const int UNSIGNED_INT = 5125;
+        public const int FLOAT = 5126;
 
         // type
         // #components=1
-        public static string SCALAR = "SCALAR";
+        public const string SCALAR = "SCALAR";
         // #components=2
-        public static string VEC2 = "VEC2";
+        public const string VEC2 = "VEC2";
         // #components=3
-        public static string VEC3 = "VEC3";
+        public const string VEC3 = "VEC3";
         // #components=4
-        public static string VEC4 = "VEC4";
+        public const string VEC4 = "VEC4";
         // #components=4
-        public static string MAT2 = "MAT2";
+        public const string MAT2 = "MAT2";
         // #components=9
-        public static string MAT3 = "MAT3";
+        public const string MAT3 = "MAT3";
         // #components=16
-        public static string MAT4 = "MAT4";
+        public const string MAT4 = "MAT4";
 
         // bufferview.target
         // array of data such as vertices
-        public static int ARRAY_BUFFER = 34962;
+        public const int ARRAY_BUFFER = 34962;
         // array of indices
-        public static int ELEMENT_ARRAY_BUFFER = 34963;
+        public const int ELEMENT_ARRAY_BUFFER = 34963;
 
-        public static uint GLB_MAGIC_BYTE = 0x46546C67;
-        public static uint CHUNK_TYPE_JSON = 0x4E4F534A;
-        public static uint CHUNK_TYPE_BINARY = 0x004E4942;
+        public const uint GLB_MAGIC_BYTE = 0x46546C67;
+        public const uint CHUNK_TYPE_JSON = 0x4E4F534A;
+        public const uint CHUNK_TYPE_BINARY = 0x004E4942;
 
-        // https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Constants
-        public static int TEXTURE_NEAREST = 0x2600;
-        public static int TEXTURE_LINEAR = 0x2601;
-        public static int TEXTURE_MAG_FILTER = 0x2800;
-        public static int TEXTURE_MIN_FILTER = 0x2801;
+        //https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/README.md#samplermagfilter
+        //https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/README.md#samplerminfilter
+        public const int NEAREST = 9728;
+        public const int LINEAR = 9729;
+        public const int NEAREST_MIPMAP_NEAREST = 9984;
+        public const int LINEAR_MIPMAP_NEAREST = 9985;
+        public const int NEAREST_MIPMAP_LINEAR = 9986;
+        public const int LINEAR_MIPMAP_LINEAR = 9987;
 
-        public static int TEXTURE_REPEAT = 0x2901;
+        public const int TEXTURE_REPEAT = 0x2901;
     }
 }
--- glTF-BinExporter/glTF/Materials/Sampler.cs
+++ glTF-BinExporter/glTF/Materials/Sampler.cs
@@ -1,12 +1,12 @@
 ﻿namespace glTF_BinExporter.glTF
 {
     /// <summary>
-    /// Defines a sampler for use in WebGL. The constants 
+    /// Defines a sampler using constants from the glTF spec
     /// </summary>
     public class Sampler
     {
-        public int magFilter = GLConstants.TEXTURE_MAG_FILTER;
-        public int minFilter = GLConstants.TEXTURE_MIN_FILTER;
+        public int magFilter = GLConstants.LINEAR;
+        public int minFilter = GLConstants.LINEAR;
         public int wrapS = GLConstants.TEXTURE_REPEAT;
         public int wrapT = GLConstants.TEXTURE_REPEAT;
     }
