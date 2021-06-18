2e9612a0e08783d79a2f28b8307ecab72042d335
Joshua Kennedy <joshuakennedy102@gmail.com>
2021-06-18 13:11:07 +0300

    Use attribute positions from draco compression information, add Draco compression object to extensions library

---
+++ glTFExtensions/KHR_draco_mesh_compression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace glTFExtensions
+{
+    public class KHR_draco_mesh_compression
+    {
+        [Newtonsoft.Json.JsonPropertyAttribute("bufferView")]
+        public int BufferView;
+
+        [Newtonsoft.Json.JsonPropertyAttribute("attributes")]
+        public Dictionary<string, int> Attributes = new Dictionary<string, int>();
+    }
+}
