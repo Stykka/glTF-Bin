072415eddac08e187bfd7929bcb6f67fb8f272f0
Joshua Kennedy <joshuakennedy102@gmail.com>
2021-08-19 13:41:51 +0300

    Add Ior and Specularity Extensions

---
+++ glTFExtensions/KHR_materials_specular.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace glTFExtensions
+{
+    public class KHR_materials_specular
+    {
+        [Newtonsoft.Json.JsonPropertyAttribute("specularFactor")]
+        public float SpecularFactor = 1.0f;
+
+        [Newtonsoft.Json.JsonPropertyAttribute("specularTexture")]
+        public glTFLoader.Schema.TextureInfo SpecularTexture = null;
+
+        [Newtonsoft.Json.JsonPropertyAttribute("specularColorFactor")]
+        public float[] SpecularColorFactor = new float[3]
+        {
+            1.0f,
+            1.0f,
+            1.0f,
+        };
+
+        [Newtonsoft.Json.JsonPropertyAttribute("specularColorTexture")]
+        public glTFLoader.Schema.TextureInfo SpecularColorTexture = null;
+
+        public bool ShouldSerializeSpecularTexture()
+        {
+            return SpecularTexture != null;
+        }
+
+        public bool ShouldSerializeSpecularColorFactor()
+        {
+            return SpecularColorFactor != null;
+        }
+
+        public bool ShouldSerializeSpecularColorTexture()
+        {
+            return SpecularColorTexture != null;
+        }
+
+    }
+}
