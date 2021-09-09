05929165a568e71e65f6a4dd77a10f3876b7c4a7
Joshua Kennedy <joshuakennedy102@gmail.com>
2021-09-09 10:26:16 +0300

    Added make package script

---
+++ make_package.sh
@@ -0,0 +1,11 @@
+if [ ! -d "./yak/" ]
+then
+    mkdir "./yak/"
+fi
+
+dd if="./glTF-BinExporter/bin/Release/net48/Newtonsoft.Json.dll" of="./yak/Newtonsoft.Json.dll"
+dd if="./glTF-BinExporter/bin/Release/net48/glTFLoader.dll" of="./yak/glTFLoader.dll"
+dd if="./glTF-BinExporter/bin/Release/net48/glTFExtensions.dll" of="./yak/glTFExtensions.dll"
+
+dd if="./glTF-BinExporter/bin/Release/net48/glTF-BinExporterWin.rhp" of="./yak/glTF-BinExporterWin.rhp"
+dd if="./glTF-BinImporter/bin/Release/glTF-BinImporter.rhp" of="./yak/glTF-BinImporter.rhp"
