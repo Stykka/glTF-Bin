c8be45274081f6ad1a634f5d947aebeb4dd7410b
Joshua Kennedy <joshuakennedy102@gmail.com>
2021-09-09 12:26:04 +0300

    Update windows exporter name

--- make_package.sh
+++ make_package.sh
@@ -7,5 +7,5 @@ dd if="./glTF-BinExporter/bin/Release/net48/Newtonsoft.Json.dll" of="./yak/Newto
 dd if="./glTF-BinExporter/bin/Release/net48/glTFLoader.dll" of="./yak/glTFLoader.dll"
 dd if="./glTF-BinExporter/bin/Release/net48/glTFExtensions.dll" of="./yak/glTFExtensions.dll"
 
-dd if="./glTF-BinExporter/bin/Release/net48/glTF-BinExporterWin.rhp" of="./yak/glTF-BinExporterWin.rhp"
+dd if="./glTF-BinExporter/bin/Release/net48/glTF-BinExporter.rhp" of="./yak/glTF-BinExporter.rhp"
 dd if="./glTF-BinImporter/bin/Release/glTF-BinImporter.rhp" of="./yak/glTF-BinImporter.rhp"
